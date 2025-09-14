ed2058cbe1c6e80044a569c6437f36e78b347fea
aman40399 <nevermind40399@gmail.com>
2025-09-14 16:32:03 +0530

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import Script from 'next/script'
+import React from 'react'
 import { SessionProvider } from './components/SessionProvider'
 
 export const metadata: Metadata = {
@@ -17,6 +19,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="font-sans bg-dark text-white overflow-x-hidden">
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6LTD8KG1KP"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6LTD8KG1KP');
+          `}
+        </Script>
+        
         <SessionProvider>
           {children}
         </SessionProvider>
